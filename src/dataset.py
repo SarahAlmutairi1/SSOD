cce5fecb694513aa8748c8ab29e88af5316b2f99
Sarah <156601492+SarahAlmutairi1@users.noreply.github.com>
2025-03-26 14:38:21 +0300

    Update dataset.py

--- src/dataset.py
+++ src/dataset.py
@@ -6,35 +6,7 @@ import os
 from pathlib import Path
 from shutil import move
 import config
-
-def convert_label(path, lb_path, year, image_id):
-    def convert_box(size, box):
-        dw, dh = 1. / size[0], 1. / size[1]
-        x, y, w, h = (box[0] + box[1]) / 2.0 - 1, (box[2] + box[3]) / 2.0 - 1, box[1] - box[0], box[3] - box[2]
-        return x * dw, y * dh, w * dw, h * dh
-
-    in_file = open(path / f'VOC{year}/Annotations/{image_id}.xml')
-    out_file = open(lb_path, 'w')
-    tree = ET.parse(in_file)
-    root = tree.getroot()
-    size = root.find('size')
-    w = int(size.find('width').text)
-    h = int(size.find('height').text)
-
-    names = [
-    "aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat",
-    "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person",
-    "pottedplant", "sheep", "sofa", "train", "tvmonitor"
-    ]
-    for obj in root.iter('object'):
-        cls = obj.find('name').text
-        if cls in names and int(obj.find('difficult').text) != 1:
-            xmlbox = obj.find('bndbox')
-            bb = convert_box((w, h), [float(xmlbox.find(x).text) for x in ('xmin', 'xmax', 'ymin', 'ymax')])
-            cls_id = names.index(cls)  # class id
-            out_file.write(" ".join(str(a) for a in (cls_id, *bb)) + '\n')
-
-
+import preprocess
 # Download
 dir = Path(f'/{HOME}/datasets/VOC')
 url = 'https://github.com/ultralytics/assets/releases/download/v0.0.0/'
