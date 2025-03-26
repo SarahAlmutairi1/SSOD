ed142764faee2ac05c2567260183dc2640b54486
Sarah <156601492+SarahAlmutairi1@users.noreply.github.com>
2025-03-26 14:48:51 +0300

    Update preprocess.py

--- src/preprocess.py
+++ src/preprocess.py
@@ -380,30 +380,3 @@ def merge_datasets(source_folders, output_folder):
                 shutil.move(label_path, target_label_path)
 
     print(f"All images and labels have been moved to {output_folder}.")
-
-def convert_label(path, lb_path, year, image_id):
-    def convert_box(size, box):
-        dw, dh = 1. / size[0], 1. / size[1]
-        x, y, w, h = (box[0] + box[1]) / 2.0 - 1, (box[2] + box[3]) / 2.0 - 1, box[1] - box[0], box[3] - box[2]
-        return x * dw, y * dh, w * dw, h * dh
-
-    in_file = open(path / f'VOC{year}/Annotations/{image_id}.xml')
-    out_file = open(lb_path, 'w')
-    tree = ET.parse(in_file)
-    root = tree.getroot()
-    size = root.find('size')
-    w = int(size.find('width').text)
-    h = int(size.find('height').text)
-
-    names = [
-    "aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat",
-    "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person",
-    "pottedplant", "sheep", "sofa", "train", "tvmonitor"
-    ]
-    for obj in root.iter('object'):
-        cls = obj.find('name').text
-        if cls in names and int(obj.find('difficult').text) != 1:
-            xmlbox = obj.find('bndbox')
-            bb = convert_box((w, h), [float(xmlbox.find(x).text) for x in ('xmin', 'xmax', 'ymin', 'ymax')])
-            cls_id = names.index(cls)  # class id
-            out_file.write(" ".join(str(a) for a in (cls_id, *bb)) + '\n')
