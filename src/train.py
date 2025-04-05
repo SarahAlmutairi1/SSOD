d5af52e2a96ee831569dd74fa58a8fa64724d65e
Sarah <156601492+SarahAlmutairi1@users.noreply.github.com>
2025-04-05 22:49:18 +0300

    Update train.py

--- src/train.py
+++ src/train.py
@@ -139,9 +139,10 @@ def Pseudo_Labeling(best_model_paths, unlabeled_dataset, iteration):
         results = model.predict(unlabeled_dataset, project=f'{HOME}/runs',save=False, save_txt=True, save_conf=True)
         print(f"Predictions for model {i+1} saved.")
 
-        # Define the path for labels source
-        labels_source = Path(f'{HOME}/runs/detect/predict/labels')
-
+        save_dir = results[0].save_dir  
+        labels_source = save_dir / 'labels'
+        print(f"Labels saved to: {labels_source}")
+        
         # Define the path for labels destination
         labels_name = f'labels-model-{model_id}'
         labels_path = os.path.join(iteration_folder_path, labels_name)
