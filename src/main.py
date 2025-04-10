f4a1cb22e87dd4e161e0c8858c47e174714cd650
Sarah <156601492+SarahAlmutairi1@users.noreply.github.com>
2025-04-10 14:21:01 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -144,6 +144,10 @@ def ETSR(iteration ,main_dataset_dir, class_names, img_size, num_instances, epoc
         print(f"Final_auto_annotated_dataset_{iteration} does not exist.")
         sys.exit()
         
+    if not os.path.exists(Final_auto_annotated_dataset):
+        print(f"Final_auto_annotated_dataset_{iteration} does not exist.")
+        sys.exit()
+        
     # Train the YOLO model using the auto-annotated labels
     model, Train_time = train.train_final_model(iteration, Final_auto_annotated_dataset, img_size, 100)
 
