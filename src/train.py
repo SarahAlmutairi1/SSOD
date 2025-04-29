0a3dbec4cf28899722e06d5ea87cc6f74d093609
Sarah <156601492+SarahAlmutairi1@users.noreply.github.com>
2025-04-29 12:08:38 +0300

    Update train.py

--- src/train.py
+++ src/train.py
@@ -26,7 +26,6 @@ def train_single_instance(dataset_path, epochs_per_iteration, model, img_size, i
           print(f"Loading yolov8n.yaml")
           model = YOLO('yolo11n.pt')
 
-
         # Train the model
         results = model.train(data=f'{dataset_path}/data.yaml', epochs=epochs_per_iteration, imgsz=img_size, plots=True, patience = 10, augment=True, project=f'{HOME}/runs') 
         save_dir = results.save_dir  # Get the save directory
@@ -45,7 +44,7 @@ def train_single_instance(dataset_path, epochs_per_iteration, model, img_size, i
         print(f"Error training model on {dataset_path}: {e}")
         traceback.print_exc()
         return None, None
-
+'''
 def train_multiple_instances(datasets, epochs_per_iteration, trained_models_paths, img_size, iteration, RetrainAll):
     """Train multiple YOLO models on different datasets in parallel using ProcessPoolExecutor."""
     best_model_paths = []
@@ -71,38 +70,22 @@ def train_multiple_instances(datasets, epochs_per_iteration, trained_models_path
 
     torch.cuda.empty_cache()  # Clear GPU memory after training
     return best_model_paths, model_performance
+
 '''
-def train_single_instance(dataset_path, epochs_per_iteration, model):
-    """Train a single YOLO instance and return the best model path and its performance."""
-    if model is not None:
-        model = YOLO(model)             # load the teacher model
-    else:
-        model = YOLO('yolov8n.pt')    # load a pretrained model
-
-    # Train the model
-    results = model.train(data=f'{dataset_path}/data.yaml', epochs=epochs_per_iteration, imgsz=img_size, plots=True, patience = 5 )
-    save_dir = results.save_dir  # Get the save directory
-    best_model_path = os.path.join(save_dir, 'weights', 'best.pt')  # Construct the path to the best model
-
-    # Retrieve and save performance metrics
-    metrics = results.maps.mean()
-
-    return best_model_path, metrics
-
-def train_multiple_instances(datasets, epochs_per_iteration, trained_models_paths, img_size):
+def train_multiple_instances(datasets, epochs_per_iteration, trained_models_paths, img_size, iteration, RetrainAll):
     """Train multiple YOLO models on different datasets and return best model paths and performance metrics."""
     best_model_paths = []
     model_performance = []
 
     # Train each model sequentially
     for dataset, model in zip(datasets, trained_models_paths):
-        best_model_path, metrics = train_single_instance(dataset, epochs_per_iteration, model, img_size)
+        best_model_path, metrics = train_single_instance(dataset, epochs_per_iteration, model, img_size, iteration, RetrainAll)
         best_model_paths.append(best_model_path)
         model_performance.append(metrics)
 
     #torch.cuda.empty_cache()  # Clear GPU memory after training
     return best_model_paths, model_performance
-'''
+
 def check_model_paths(paths):
   '''
   function to delete any extra folders that could be created during parallel training
